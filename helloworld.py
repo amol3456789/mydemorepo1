3dc5f2d9b7dc124d0207563fbf3ac0d4a952c772
amol3456789 <56922652+amol3456789@users.noreply.github.com>
2020-07-23 13:16:23 +0530

    Update helloworld.py
    yes

--- helloworld.py
+++ helloworld.py
@@ -5,13 +5,6 @@ print("Hello World!")
 print("This is line 2")
 print("this is line 3")
 
-
-print("Hello World!")
-print("This is line 2")
-print("this is line 3")
-
-a = 10
-b = 20
-c=a+b
+END of line
 
 
